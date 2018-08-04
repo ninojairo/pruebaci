e23f99cc199c84ad063272e9717278285c9de61b
ninojairo <nino.jairo@gmail.com>
2018-08-04 08:56:49 -0500

    test updated

--- src/test/java/com/bancodebogota/fdm/pruebaci/pruebaciTest.java
+++ src/test/java/com/bancodebogota/fdm/pruebaci/pruebaciTest.java
@@ -23,10 +23,10 @@ public class pruebaciTest {
     @Test
     public void testSum() {
         System.out.println("sum");
-        int n1 = 0;
-        int n2 = 0;
+        int n1 = 1;
+        int n2 = 2;
         pruebaci instance = new pruebaci();
-        int expResult = 0;
+        int expResult = 3;
         int result = instance.sum(n1, n2);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
