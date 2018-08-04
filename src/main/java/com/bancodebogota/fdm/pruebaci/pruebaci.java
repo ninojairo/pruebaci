f512bfdc601c73dae50b414122990a79ee516980
ninojairo <nino.jairo@gmail.com>
2018-08-04 08:44:20 -0500

    first commit

---
+++ src/main/java/com/bancodebogota/fdm/pruebaci/pruebaci.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bancodebogota.fdm.pruebaci;
+
+/**
+ *
+ * @author jaironino
+ */
+public class pruebaci {
+    
+    public int sum(int n1, int n2){
+        return n1+n2;
+    }
+    
+    public static void main(String[] args) {
+        System.err.println("TESTCI");
+    }
+    
+}
